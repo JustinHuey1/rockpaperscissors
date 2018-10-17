6ea8efaba4dcfc041c95a8fdff0a47dfb3f26d0e
Justin Huey <juhuey1@s.sfusd.edu>
2018-10-17 01:02:56 +0000

    Made game work

--- js/script.js
+++ js/script.js
@@ -6,16 +6,55 @@
 
 
 
-
+/*global $*/
 // DOCUMENT READY FUNCTION BELOW
 $(document).ready(function(){
-    var CC = ["paper", "rock", "scissor"];
+    var randomNumber = 0;
+    var userChoice = "";
+    var computerChoice = "";
+    var winner = "";
     $("button").click(function(){
-        $("#userChoice").text($("#input").val());
-        for(var i=0; i = CC.length; i++ ){
-            
-            
+        var userChoice = $("#input").val()
+        $("#userChoice").text(userChoice);
+        var randomNumber = Math.random();
+        if(randomNumber <= .33){
+            var computerChoice = "rock";
+        }else if(randomNumber <= .66 && randomNumber >= .33){
+            var computerChoice = "paper";
+        }else if(randomNumber  <= 1 && randomNumber >= .66){
+            var computerChoice = "scissor";
         }
-        $("#computerChoice").text(CC);
+        $("#computerChoice").text(computerChoice);
+        if(userChoice === "rock" && computerChoice === "rock"){
+            $("#winner").text("Tie");
+        }else if(userChoice === "rock" && computerChoice === "paper"){
+            $("#winner").text("Computer Wins");
+        }else if(userChoice === "rock" && computerChoice === "scissor"){
+            $("#winner").text("User Wins");
+        }else if(userChoice === "paper" && computerChoice === "rock"){
+            $("#winner").text("User Wins");
+        }else if(userChoice === "paper" && computerChoice === "paper"){
+            $("#winner").text("Tie");
+        }else if(userChoice === "paper" && computerChoice === "scissor"){
+            $("#winner").text("Computer Wins");
+        }else if(userChoice === "scissor" && computerChoice === "rock"){
+            $("#winner").text("Computer Wins");
+        }else if(userChoice === "scissor" && computerChoice === "paper"){
+            $("#winner").text("User Wins");
+        }else if(userChoice === "scissor" && computerChoice === "scissor"){
+            $("#winner").text("Tie");
+        }else{
+        }
+        var first = 0;
+        var second = 0;
+        $("button").click(function(){
+            if($("#winner").text() === "User Wins"){
+                var first = first + 1;
+            }else if($("#winner").text() === "Computer Wins"){
+                var second = second + 1;
+            }else{
+            }
+            
+        });
     });
 });
